ee20d623976a507633a031b4b8f500ea0abdbd1f
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-12-30 14:16:25 +0530

    ADDED:Welcome to Indian States Census Analyser

---
+++ src/main/java/com/bridgelabz/IndianStreet/StateCensusAnalyser.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.IndianStreet;
+
+public class StateCensusAnalyser {
+
+    public static void main( String[] args ) {
+        System.out.println( "Welcome to Indian States Census Analyser " );
+    }
+}
